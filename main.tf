c6cf2a34d394893ba4af006d84af16765c43698d
tabishshakil123 <tshakil123@gmail.com>
2025-08-20 19:09:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,4 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }azure resource group is logical container in microsoft azure that hold related resources for an application or solution
 it used to organize and mange azure resources
+all resources in the resource group can deployed updated and deleted together
